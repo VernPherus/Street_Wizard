c7f4cc530f31a8582b0b370bfcf6536f9884924f
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2025-01-20 13:47:55 +0800

    added new script for cam, fixed playerCam script

--- Assets/Scripts/PlayerScripts/playerCam.cs
+++ Assets/Scripts/PlayerScripts/playerCam.cs
@@ -8,7 +8,7 @@ public class player_Cam : MonoBehaviour
     public float sensX = 100f;
     public float sensY = 100f;
 
-    public Transform playerBody;
+    public Transform orientation;
 
     float xRotation;
     float yRotation;
@@ -23,10 +23,12 @@ public class player_Cam : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
 
-        xRotation = mouseX;
-        yRotation = mouseY;
+        yRotation += mouseX;
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        playerBody.Rotate(Vector3.up * mouseX);    
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
