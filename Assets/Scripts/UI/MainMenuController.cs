502bddc7f45eacfdb398cb680a319bd8c8eb37bb
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2025-03-24 11:34:10 +0800

    added ui scripts, edited mainmenu controller

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,50 +1,112 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
-    public VisualElement ui;
+    [SerializeField]
+    private GameObject Player;
+    [SerializeField]
+    private GameObject PlayerHUD;
+    [SerializeField]
+    private GameObject PlayerCamera;
+    [SerializeField]
+    private GameObject MenuCamera;
 
-    public Button playButton;
-    public Button settingsButton;
-    public Button quitButton;
+    [SerializeField]
+    private GameObject PauseMenu;
+
+    public void OnEscape()
+    {
+        Player.SetActive(false);
+        PlayerHUD.SetActive(false);
+
+        Time.timeScale = 0;
+
+        PauseMenu.SetActive(true);
+    }
 
     private void Awake()
     {
-        ui = GetComponent<UIDocument>().rootVisualElement;
+        Player.SetActive(false);
+        PlayerHUD.SetActive(false);
+        PauseMenu.SetActive(false);
     }
-
-    private void OnEnable()
+    
+    public void PlayGame()
     {
-        playButton = ui.Q<Button>("PlayButton");
-        playButton.clicked += OnPlayClicked;
+        Player.SetActive(true);
+        PlayerCamera.SetActive(true);
+        PlayerHUD.SetActive(true);
 
-        settingsButton = ui.Q<Button>("SettingsButton");
-        settingsButton.clicked += OnSettingsClicked;
-
-        quitButton = ui.Q<Button>("QuitButton");
-        quitButton.clicked += OnQuitClicked;
+        MenuCamera.SetActive(false);
     }
 
-
-    private void OnPlayClicked()
-    {
-        Debug.Log("PLAY!");
-    }
-
-    private void OnSettingsClicked()
-    {
-        Debug.Log("OPTIONS!");
-    }
-
-    private void OnQuitClicked()
+    public void QuitGame()
     {
         Application.Quit();
 #if UNITY_EDITOR
-    EditorApplication.isPlaying = false;
+        EditorApplication.isPlaying = false;
 #endif
     }
+
+    public void ResumeGame()
+    {
+        Player.SetActive(true);
+        PlayerHUD.SetActive(true);
+
+        Time.timeScale = 1;
+
+        PauseMenu.SetActive(false);
+    }
+
+    public void ReturnToMenu()
+    {
+        Player.SetActive(false);
+        PlayerCamera.SetActive(false);
+        PlayerHUD.SetActive(false);
+
+        MenuCamera.SetActive(true);
+    }
+
+    //     public VisualElement ui;
+
+    //     public Button playButton;
+    //     public Button settingsButton;
+    //     public Button quitButton;
+
+    //     private void Awake()
+    //     {
+    //         ui = GetComponent<UIDocument>().rootVisualElement;
+    //     }
+
+    //     private void OnEnable()
+    //     {
+    //         playButton = ui.Q<Button>("PlayButton");
+    //         playButton.clicked += OnPlayClicked;
+
+    //         settingsButton = ui.Q<Button>("SettingsButton");
+    //         settingsButton.clicked += OnSettingsClicked;
+
+    //         quitButton = ui.Q<Button>("QuitButton");
+    //         quitButton.clicked += OnQuitClicked;
+    //     }
+
+
+    //     private void OnPlayClicked()
+    //     {
+    //         Debug.Log("PLAY!");
+    //     }
+
+    //     private void OnSettingsClicked()
+    //     {
+    //         Debug.Log("OPTIONS!");
+    //     }
+
+    //     private void OnQuitClicked()
+    //     {
+    //         Application.Quit();
+    // #if UNITY_EDITOR
+    //     EditorApplication.isPlaying = false;
+    // #endif
+    //     }
 }
